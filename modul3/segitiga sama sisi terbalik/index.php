529ea48a4eb4b66276b7475ab0f88a342ad06395
lucxifvr <148960358+ramaprastaruna@users.noreply.github.com>
2024-10-25 15:30:22 +0700

    Add files via upload

---
+++ modul3/segitiga sama sisi terbalik/index.php
@@ -0,0 +1,13 @@
+<?php
+$rows = 5;
+
+for ($i = $rows; $i >= 1; $i--) {
+    for ($j = 1; $j <= $rows - $i; $j++) {
+        echo "&nbsp;&nbsp;";
+    }
+    for ($k = 1; $k <= (2 * $i - 1); $k++) {
+        echo "*";
+    }
+    echo "<br>";
+}
+?>
